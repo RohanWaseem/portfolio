3d95c96c14e59fa38f42d2a069a1767797158a50
Rohan Waseem <rohanwaseem985@gmail.com>
2026-06-05 04:10:33 -0700

    Updated portfolio

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
 <div className='pl-10 pr-10 flex  gap-5  items-center justify-center  flex-wrap'>
 
 
-    <div className='bg-slate-900 flex flex-col lg:h-90 hover:border-cyan-400 z-50  hover:border-2 transition-transform duration-300 hover:-translate-y-1 rounded-2xl justify-center  items-center h-50 mt-10 sm:w-[46%] sm:h-70 lg:w-[30%]'>
+    <div className='bg-slate-900 flex flex-col lg:h-90 hover:border-cyan-400 z-10  hover:border-2 transition-transform duration-300 hover:-translate-y-1 rounded-2xl justify-center  items-center h-60 mt-10 sm:w-[46%] sm:h-70 lg:w-[30%]'>
     <div>
     <h1 className='text-2xl text-sky-400 font-bold'>Decode Lab</h1></div>
 
